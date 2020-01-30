c88bac58a38ba854b583439af1fb0c0f3c1feb73
798150 <798150@seq.org>
2020-01-30 08:50:35 -0800

    g

---
+++ Lab 0128 Art One/sketch.js
@@ -0,0 +1,33 @@
+// Kyle Knudson
+// 	1/29
+//  This is a comment
+//  The setup function function is called once when your program begins
+var Boids=[];
+
+function setup() {
+  // put setup code here
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(255, 240, 0);
+  fill(20, 230, 150);
+  //loads boids
+  loadBoids(100);
+}
+
+function draw() {
+  background(230,120,200);
+  //runs Boids
+  runBoids();
+}
+
+function loadBoids(n){
+  for(var i=0; i<n; i++){
+    Boids[i]= new Boid(random(width), random(height),random(-8,8), random(-8,8));
+  }
+}
+
+function runBoids(){
+  for(var i=0; i<Boids.length; i++){
+    Boids[i].run();
+  }
+}
